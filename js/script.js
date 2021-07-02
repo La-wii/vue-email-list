eab65c6fab5143470ae7441fae331be023ceca59
La-wii <sentilavale@gmail.com>
2021-07-02 16:30:19 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 const app = new Vue({
-    el:"#app"
+    el:"#app",
+
+    data : {
+        arrayMail : []
+    },
+
+    mounted() {
+
+        for(let i = 0; i < 10; i++){
+
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(element => {
+                // console.log(element.data.response);
+                this.arrayMail.push(element.data.response);
+                // console.log(this.arrayMail);
+            })
+        }
+    },
 })
